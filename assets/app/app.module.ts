3becc7a14e017f8b9763f63f302e50aa51fd2a12
Unknown <prasanthi@itversity.in>
2017-08-29 16:50:08 +0530

    Home and Employee routes added

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,21 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { EmployeeService } from './employee.service';
-import { EmployeeComponent } from './employee/employee.component'
+import { EmployeeComponent } from './employee/employee.component';
+import { HomeComponent } from './home/home.component';
+
+import { EmployeeService } from './services/employee.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeComponent
+    EmployeeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+        {
+          path: '',
+          component: HomeComponent
+        },
+        {
+          path: 'employee',
+          component: EmployeeComponent
+        }
+      ])
   ],
   providers: [
     EmployeeService
---
+++ assets/app/models/employee.ts
@@ -0,0 +1,8 @@
+export class Employee {
+	constructor(
+		public _id: string,
+		public firstName: string,
+		public lastName: string,
+		public salary: number
+		) { }
+}
